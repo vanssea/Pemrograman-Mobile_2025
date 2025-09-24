6aa422efaabeac585df7099b56c7f19e5be8b887
vanssea <cristinvanessa6@gmail.com>
2025-09-24 12:20:34 +0700

    Modifikasi Tugas Praktikum 1 - Eksperimen Tipe Data List

--- P4/p4.dart
+++ P4/p4.dart
@@ -1,12 +1,23 @@
-// Praktikum 1 - Eksperimen Tipe Data List
-void main() {
-  var list = [1, 2, 3];
-  assert(list.length == 3);
-  assert(list[1] == 2);
-  print(list.length);
-  print(list[1]);
+// // Praktikum 1 - Eksperimen Tipe Data List
+// void main() {
+//   var list = [1, 2, 3, 4, 5];
+//   assert(list.length == 5);
+//   assert(list[1] == 2);
+//   print(list.length);
+//   print(list[1]);
 
-  list[1] = 1;
-  assert(list[1] == 1);
-  print(list[1]);
+//   list[1] = 1;
+//   assert(list[1] == 1);
+//   print(list[1]);
+// }
+
+// Modifikasi Tugas Praktikum 1 - Eksperimen Tipe Data List
+void main() {
+  final List<dynamic> list = List.filled(5, null);
+
+  list[1] = "Vanessa Cristin Natalia";
+  list[2] = "2341720026";
+
+  print("Nama: ${list[1]}");
+  print("NIM: ${list[2]}");
 }
