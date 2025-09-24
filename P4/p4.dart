0a560223d033e6d81c30daf9cc3e0365ce953d5b
vanssea <cristinvanessa6@gmail.com>
2025-09-24 11:21:45 +0700

    Praktikum 1: Eksperimen Tipe Data List

---
+++ P4/p4.dart
@@ -0,0 +1,12 @@
+// Praktikum 1
+void main() {
+  var list = [1, 2, 3];
+  assert(list.length == 3);
+  assert(list[1] == 2);
+  print(list.length);
+  print(list[1]);
+
+  list[1] = 1;
+  assert(list[1] == 1);
+  print(list[1]);
+}
