694576f0860bdd3ababc4e08cc01bb58ec9554ff
vanssea <cristinvanessa6@gmail.com>
2025-09-24 13:05:08 +0700

    Pertemuan 4 - Praktikum 2

--- P4/p4.dart
+++ P4/p4.dart
@@ -11,13 +11,34 @@
 //   print(list[1]);
 // }
 
-// Modifikasi Tugas Praktikum 1 - Eksperimen Tipe Data List
+// // Modifikasi Tugas Praktikum 1 - Eksperimen Tipe Data List
+// void main() {
+//   final List<dynamic> list = List.filled(5, null);
+
+//   list[1] = "Vanessa Cristin Natalia";
+//   list[2] = "2341720026";
+
+//   print("Nama: ${list[1]}");
+//   print("NIM: ${list[2]}");
+// }
+
+// // Tugas Praktikum 2 - Eksperimen Tipe Data Set
+// void main() {
+//   var halogen = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
+
+//   print(halogen);
+// }
+
+// Modifikasi Tugas Praktikum 2 - Eksperimen Tipe Data Set
 void main() {
-  final List<dynamic> list = List.filled(5, null);
+  var names1 = <String>{};
+  Set<String> names2 = {};
 
-  list[1] = "Vanessa Cristin Natalia";
-  list[2] = "2341720026";
 
-  print("Nama: ${list[1]}");
-  print("NIM: ${list[2]}");
+  names1.add("Vanessa Cristin Natalia");
+  names2.add("2341720026");
+  names2.addAll(["Vanessa Cristin Natalia", "2341720026"]);
+  
+  print(names1);
+  print(names2);
 }
